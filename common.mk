c9af6cec222a070cd9eb88361a5b7c57e8396bf8
Dylan Neve <dylanneve1@gmail.com>
2023-06-20 17:45:35 -0600

    exynos9810-common: build Lineage touch HAL
    Used for glove mode, accessible via settings app.

--- common.mk
+++ common.mk
@@ -369,6 +369,10 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
     android.hardware.thermal@1.0-service
 
+# Touch
+PRODUCT_PACKAGES += \
+    vendor.lineage.touch@1.0-service.exynos9810
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb-service.exynos9810
